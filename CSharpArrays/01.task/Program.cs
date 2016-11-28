b92b11a2eaedfa6011ad7398cf5a6ccbddc5109e
radosvet93 <93radosvet@gmail.com>
2016-11-28 10:53:51 +0000

    Add files via upload

---
+++ CSharpArrays/01.task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] multiplyByFive = new int[20];
+
+            for (int i = 0; i < multiplyByFive.Length; i++)
+            {
+                multiplyByFive[i] = i;
+            }
+            foreach (var item in multiplyByFive)
+            {
+                Console.WriteLine(item * 5);
+            }
+        }
+    }
+}
---
+++ CSharpArrays/05.task/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 2, 3, 4, 5, 2, 2, 4, 5, 6, 7 };
+
+            int count = 1;
+            int longestSequence = 0;
+            string longestSequenceNum = "";
+            string sequenceNum = "";
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] < arr[i + 1])
+                {
+                    count++;
+                    sequenceNum += arr[i] + " ";
+                }
+                else
+                {
+                    if (longestSequence < count)
+                    {
+                        sequenceNum += arr[i] + " ";
+                        longestSequence = count;
+                        longestSequenceNum = sequenceNum;
+                    }
+                    count = 1;
+                    sequenceNum = "";
+                }
+            }
+            //for the last element
+            if (longestSequence < count)
+            {
+                longestSequenceNum += arr[arr.Length - 1] + " ";
+                longestSequence = count;
+            }
+            //write it on the console
+            Console.WriteLine("The longest sequence is array of these numbers {0}", longestSequenceNum);
+
+        }
+    }
+}
+
